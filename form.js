c23e52077a56314f6156e837daf8e61a472a32d7
Kirina Mikhadyuk <mikhadyuk@gmail.com>
2022-11-22 13:57:01 +0300

    form

---
+++ form.js
@@ -0,0 +1,21 @@
+function handleAddBook () {
+     const formData = document.querySelector('#form').elements;
+
+     const json = {};
+     for (let i = 0; i < formData.length; i++) {
+          json[formData[i].id] = formData[i].value;
+     }
+
+     fetch('https://book-service-mikhadyuk.herokuapp.com/book', {
+          method: 'POST',
+          headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json',
+               'Access-Control-Allow-Origin': 'https://book-service-mikhadyuk.herokuapp.com/book'
+          },
+          body: json
+     })
+         .then(response => console.log(response.json()))
+         .catch(error => console.log(error));
+
+}
