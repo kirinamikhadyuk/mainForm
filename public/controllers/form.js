799c21971b7f4348635216d9fa9ceebb3e5a14d6
Kirina Mikhadyuk <mikhadyuk@gmail.com>
2022-11-23 18:04:22 +0300

    changes

--- public/controllers/form.js
+++ public/controllers/form.js
@@ -1,22 +1,84 @@
-function handleAddBook () {
-     const formData = document.querySelector('#form').elements;
+let selectedBook;
+let selectedCustomer;
 
-     const json = {};
-     for (let i = 0; i < formData.length; i++) {
-          json[formData[i].id] = formData[i].value;
-     }
-     console.log(json);
+function init() {
+     const booksDropDown = document.getElementById("books");
+     const customersDropDown = document.getElementById("customers");
 
-     fetch('https://book-service-mikhadyuk.herokuapp.com/book', {
-          method: 'POST',
-          mode: "cors",
-          headers: {
-               'Accept': 'application/json',
-               'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(json)
-     })
-         .then(response => console.log(response))
-         .catch(error => console.log(error));
+    Promise.all([
+        fetch('https://book-service-mikhadyuk.herokuapp.com/allBooks', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                generateDropdownOptions(booksDropDown, response, 'title');
+            })
+            .catch(error => console.log(error)),
+        fetch('https://customer-service-mikhadyuk.herokuapp.com/allCustomers', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                generateDropdownOptions(customersDropDown, response, 'name');
+            })
+            .catch(error => console.log(error))])
+        .then(() => {
+            document.getElementById('container').classList.toggle('hide');
+            document.getElementById('loader').classList.toggle('hide');
+        })
+}
 
+function generateDropdownOptions(dropdownElement, options, fieldTitle) {
+    for (let i = 0; i < options.length; i++) {
+        const option = document.createElement("option");
+        option.setAttribute('value', options[i][fieldTitle]);
+        option.setAttribute('_id', options[i]._id);
+
+        const optionText = document.createTextNode(options[i][fieldTitle]);
+        option.appendChild(optionText);
+
+        dropdownElement.appendChild(option);
+    }
+}
+
+function saveOrder() {
+    selectedBook = [...document.getElementById('books').options].find(item => item.selected);
+    selectedCustomer = [...document.getElementById('customers').options].find(item => item.selected);
+
+    const requestBody = {
+        bookId: selectedBook.getAttribute('_id'),
+        customerId: selectedCustomer.getAttribute('_id'),
+        initialDate: Date.now(),
+        deliveryDate: Date.now() + 10 * 24 * 60 * 60 * 1000
+    };
+
+    fetch('https://order-service-mikhadyuk.herokuapp.com/order', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => {
+            document.getElementById('container').classList.toggle('hide');
+            document.getElementById('finish').classList.toggle('hide');
+        })
+        .catch(error => console.log(error))
+}
+
+function openOrderTemplate() {
+    selectedBook.selected = false;
+    selectedCustomer.selected = false;
+
+    document.getElementById('container').classList.toggle('hide');
+    document.getElementById('finish').classList.toggle('hide');
 }
